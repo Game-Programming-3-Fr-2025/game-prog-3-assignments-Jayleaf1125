340deb27c9e0b6ea5a90f86ad17f001fd166ca98
MJOJO63 <84742412+Jayleaf1125@users.noreply.github.com>
2025-09-19 17:04:13 -0400

    Some additions to proto 4

--- Assets/Prototype 2/Scripts/PlayerMovement.cs
+++ Assets/Prototype 2/Scripts/PlayerMovement.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Moving")]
     public float speed;
+
+    [Header("Jumping")]
+    public float jumpForce;
+    bool _isGrounded = true;
 
     Rigidbody2D _rb;
     SpriteRenderer _sr;
@@ -23,10 +28,31 @@ public class PlayerMovement : MonoBehaviour
         _vm.x = Input.GetAxisRaw("Horizontal");
 
         if (isVerticalMovementOn) _vm.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
     }
 
     private void FixedUpdate()
     {
         _rb.MovePosition(_rb.position + _vm * speed * Time.fixedDeltaTime);
+    }
+
+    void Jump()
+    {
+        if (_isGrounded)
+        {            
+            Debug.Log("Read Jump Function");
+            _rb.AddForce(new Vector2(_rb.linearVelocity.x, jumpForce), ForceMode2D.Impulse);
+            _isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If the object's layer is the Ground layer (6)
+        if(collision.gameObject.layer == 6)
+        {
+            _isGrounded = true;
+        }
     }
 }
